31d4a944af3bedd28c2c62dff02bdb1ca5b3406e
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-06 17:11:00 +0600

    add addEventListener

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let Click =document.querySelector('#click');
+let h1 = document.querySelector("h1");
+let sayHello = document.querySelector("hello");
+let input = document.querySelector("#change");
+let color = document.querySelector('#color');
+let puls = document.querySelector('#minus');
+let h2 = document.querySelector('h2');
+
+Click.addEventListener('click',function(){
+    h1.innerText = "Chlicked"
+})
+Click.addEventListener('click',function(){
+    h1.innerText = "Chlicked"
+})
